93ebcd8b5e702a8fa72a166d90b8e34dfc12f9bb
Alexander SB <alexsb12348@gmail.com>
2017-12-03 22:11:00 -0500

    lab14

---
+++ Lab 14/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lab_14.Startup))]
+namespace Lab_14
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
